fe112196a11457e29bca5817cc487aea05b86bcb
SangJune-Kim <ghfnsgldl@gmail.com>
2022-05-11 11:34:20 +0900

    Setting

---
+++ f6b2-team2-client/src/commons/styles/globalStyles.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+  // @font-face {
+  // font-family : 글꼴
+  // src: url("글꼴위치")
+  //}
+`;
---
+++ f6b2-team2-client/src/components/commons/layout/banner/index.tsx
@@ -0,0 +1,7 @@
+export default function LayoutBanner() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
