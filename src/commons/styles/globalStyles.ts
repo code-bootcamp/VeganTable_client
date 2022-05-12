ca746ea000421fe6a9642d1efb029f85bcbfffb8
KKingmo <55ooccmm@gmail.com>
2022-05-12 12:06:06 +0900

    Setting: [#14] 폰트

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -3,10 +3,94 @@ import { css } from "@emotion/react";
 export const globalStyles = css`
   * {
     box-sizing: border-box;
-    font-size: 16px;
+    font-family: "Pretendard", "돋움", sans-serif;
+    margin: 0;
+    padding: 0;
   }
-  // @font-face {
-  // font-family : 글꼴
-  // src: url("글꼴위치")
-  //}
+
+  html {
+    font-size:16px;
+  }
+
+/* Pretendard */
+@font-face {
+    font-family: "Pretendard";
+    font-weight: 900;
+    font-display: swap;
+    src: local("Pretendard Black"),
+      url("/fonts/Pretendard-Black.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-Black.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 800;
+    font-display: swap;
+    src: local("Pretendard ExtraBold"),
+      url("/fonts/Pretendard-ExtraBold.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-ExtraBold.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 700;
+    font-display: swap;
+    src: local("Pretendard Bold"),
+      url("/fonts/Pretendard-Bold.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-Bold.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 600;
+    font-display: swap;
+    src: local("Pretendard SemiBold"),
+      url("/fonts/Pretendard-SemiBold.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-SemiBold.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 500;
+    font-display: swap;
+    src: local("Pretendard Medium"),
+      url("/fonts/Pretendard-Medium.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-Medium.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 400;
+    font-display: swap;
+    src: local("Pretendard Regular"),
+      url("/fonts/Pretendard-Regular.subset.woff2") format("woff2"),
+      url("/fonts/woff-subset/Pretendard-Regular.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 300;
+    font-display: swap;
+    src: local("Pretendard Light"),
+      url("/fonts/Pretendard-Light.subset.woff2") format("woff2"),
+      url("/fonts/Pretendard-Light.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 200;
+    font-display: swap;
+    src: local("Pretendard ExtraLight"),
+      url("/fonts/Pretendard-ExtraLight.subset.woff2") format("woff2"),
+      url("/fonts/Pretendard-ExtraLight.subset.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 100;
+    font-display: swap;
+    src: local("Pretendard Thin"),
+      url("/fonts/Pretendard-Thin.subset.woff2") format("woff2"),
+      url("/fonts/Pretendard-Thin.subset.woff") format("woff");
+  }
 `;
