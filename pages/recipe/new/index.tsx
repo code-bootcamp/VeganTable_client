8b0189844738a39eb3457f528a5fedd2faf803da
KKingmo <55ooccmm@gmail.com>
2022-05-17 21:24:05 +0900

    Feat: [#67] 메인페이지 useInView적용, 레시피 등록 폴더만 생성

---
+++ pages/recipe/new/index.tsx
@@ -0,0 +1,5 @@
+import RecipeWrite from "../../../src/components/units/recipe/write/RecipeWrite.container";
+
+export default function RecipeWritePage() {
+  return <RecipeWrite />;
+}
---
+++ src/components/units/recipe/write/RecipeWrite.container.tsx
@@ -0,0 +1,5 @@
+import RecipeWriteUI from "./RecipeWrite.presenter";
+
+export default function RecipeWrite() {
+  return <RecipeWriteUI />;
+}
