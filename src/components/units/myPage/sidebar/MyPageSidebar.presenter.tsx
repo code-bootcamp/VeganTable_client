56d6b221d2399132167e211f118f3da7e4169c21
hhjk00 <eunyu0604@gmail.com>
2022-05-17 15:52:05 +0900

    Modify: [#59] 마이페이지 레이아웃

--- src/components/units/myPage/sidebar/MyPageSidebar.presenter.tsx
+++ src/components/units/myPage/sidebar/MyPageSidebar.presenter.tsx
@@ -1,18 +1,53 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
 import * as My from "./MyPageSidebar.styles";
 
 export default function MyPageSidebarUI() {
+  const router = useRouter();
+
+  const MYPAGE_MENUS = [
+    { name: "MY 홈", page: "/myPage" },
+    { name: "레시피관리", page: "" },
+    { name: "최근 본 레시피", page: "/myPage/recipe/recent", list: true },
+    { name: "찜한 레시피", page: "/myPage/recipe/heart", list: true },
+    { name: "등록 레시피 관리", page: "/myPage/recipe/myList", list: true },
+    { name: "정기 구독 관리", page: "/myPage/subscribe" },
+    { name: "문의 내역", page: "/myPage/qna" },
+    { name: "회원 정보 수정", page: "/myPage/edit" },
+  ];
+
   return (
     <My.Container>
       <My.Wrapper>
-        <>My 홈</>
-        <span>레시피 관리</span>
-        <span>최근 본 레시피</span>
-        <span>찜한 레시피</span>
-        <span>좋아요 한 레시피</span>
-        <span>등록 레시피 관리</span>
-        <span>정기 구독 관리</span>
-        <span>문의 내역</span>
-        <span>회원 정보 수정</span>
+        {MYPAGE_MENUS.map((el) => (
+          <Fragment key={el.page}>
+            {!el.list &&
+              (el.name === "레시피관리" ? (
+                <My.MenuItem1>{el.name}</My.MenuItem1>
+              ) : (
+                <Link href={el.page}>
+                  <My.MenuItem1
+                    id={el.page}
+                    className={router.asPath === el.page ? "isActive" : ""}
+                  >
+                    {el.name}
+                  </My.MenuItem1>
+                </Link>
+              ))}
+
+            {el.list && (
+              <Link href={el.page}>
+                <My.MenuItem2
+                  id={el.page}
+                  className={router.asPath === el.page ? "isActive" : ""}
+                >
+                  {el.name}
+                </My.MenuItem2>
+              </Link>
+            )}
+          </Fragment>
+        ))}
       </My.Wrapper>
     </My.Container>
   );
--- src/components/units/myPage/sidebar/MyPageSidebar.styles.ts
+++ src/components/units/myPage/sidebar/MyPageSidebar.styles.ts
@@ -9,5 +9,27 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   width: 6.6875rem;
-  height: 28.25rem;
+  height: 22.25rem;
+`;
+
+export const MenuItem1 = styled.ul`
+  font-size: 1.25rem;
+  font-weight: 500;
+  letter-spacing: -0.06rem;
+  cursor: pointer;
+
+  &.isActive {
+    color: #0fbaa3;
+  }
+`;
+
+export const MenuItem2 = styled.li`
+  font-weight: 500;
+  letter-spacing: -0.06rem;
+  color: #848484;
+  cursor: pointer;
+
+  &.isActive {
+    color: #0fbaa3;
+  }
 `;
