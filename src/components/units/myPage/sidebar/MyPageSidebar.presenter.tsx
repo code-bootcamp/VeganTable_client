02b0cf205762c97554593fa34f66e976bc0b4ce6
hhjk00 <eunyu0604@gmail.com>
2022-05-17 15:53:54 +0900

    Modify: [#59] 마이페이지 레이아웃

--- src/components/units/myPage/sidebar/MyPageSidebar.presenter.tsx
+++ src/components/units/myPage/sidebar/MyPageSidebar.presenter.tsx
@@ -9,9 +9,9 @@ export default function MyPageSidebarUI() {
   const MYPAGE_MENUS = [
     { name: "MY 홈", page: "/myPage" },
     { name: "레시피관리", page: "" },
-    { name: "최근 본 레시피", page: "/myPage/recipe/recent", list: true },
-    { name: "찜한 레시피", page: "/myPage/recipe/heart", list: true },
-    { name: "등록 레시피 관리", page: "/myPage/recipe/myList", list: true },
+    { name: "최근 본 레시피", page: "/myPage/recent", list: true },
+    { name: "찜한 레시피", page: "/myPage/wish", list: true },
+    { name: "등록 레시피 관리", page: "/myPage/myRecipe", list: true },
     { name: "정기 구독 관리", page: "/myPage/subscribe" },
     { name: "문의 내역", page: "/myPage/qna" },
     { name: "회원 정보 수정", page: "/myPage/edit" },
@@ -20,34 +20,38 @@ export default function MyPageSidebarUI() {
   return (
     <My.Container>
       <My.Wrapper>
-        {MYPAGE_MENUS.map((el) => (
-          <Fragment key={el.page}>
-            {!el.list &&
-              (el.name === "레시피관리" ? (
-                <My.MenuItem1>{el.name}</My.MenuItem1>
-              ) : (
+        <ul>
+          {MYPAGE_MENUS.map((el) => (
+            <Fragment key={el.page}>
+              {!el.list &&
+                (el.name === "레시피관리" ? (
+                  <My.MenuItem1>
+                    <span>{el.name}</span>
+                  </My.MenuItem1>
+                ) : (
+                  <Link href={el.page}>
+                    <My.MenuItem1
+                      id={el.page}
+                      className={router.asPath === el.page ? "isActive" : ""}
+                    >
+                      <a>{el.name}</a>
+                    </My.MenuItem1>
+                  </Link>
+                ))}
+
+              {el.list && (
                 <Link href={el.page}>
-                  <My.MenuItem1
+                  <My.MenuItem2
                     id={el.page}
                     className={router.asPath === el.page ? "isActive" : ""}
                   >
-                    {el.name}
-                  </My.MenuItem1>
+                    <a>{el.name}</a>
+                  </My.MenuItem2>
                 </Link>
-              ))}
-
-            {el.list && (
-              <Link href={el.page}>
-                <My.MenuItem2
-                  id={el.page}
-                  className={router.asPath === el.page ? "isActive" : ""}
-                >
-                  {el.name}
-                </My.MenuItem2>
-              </Link>
-            )}
-          </Fragment>
-        ))}
+              )}
+            </Fragment>
+          ))}
+        </ul>
       </My.Wrapper>
     </My.Container>
   );
--- src/components/units/myPage/sidebar/MyPageSidebar.styles.ts
+++ src/components/units/myPage/sidebar/MyPageSidebar.styles.ts
@@ -7,15 +7,15 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   width: 6.6875rem;
   height: 22.25rem;
 `;
 
-export const MenuItem1 = styled.ul`
+export const MenuItem1 = styled.li`
   font-size: 1.25rem;
   font-weight: 500;
   letter-spacing: -0.06rem;
+  padding: 1rem 0;
   cursor: pointer;
 
   &.isActive {
@@ -27,6 +27,8 @@ export const MenuItem2 = styled.li`
   font-weight: 500;
   letter-spacing: -0.06rem;
   color: #848484;
+  padding: 0.8rem 0;
+
   cursor: pointer;
 
   &.isActive {
