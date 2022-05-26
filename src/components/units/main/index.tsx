57d3e2886a9d493a99c0ced3b19616946056823e
KKingmo <55ooccmm@gmail.com>
2022-05-26 15:54:56 +0900

    Feat: [#175] scroll to Top 구현

--- src/components/units/main/index.tsx
+++ src/components/units/main/index.tsx
@@ -7,6 +7,9 @@ import Magazine from "./magazine/Magazine.container";
 import Promotion from "./promotion/Promotion.container";
 
 export default function Main() {
+  const onClickTopButton = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <>
       <SearchBar01 />
@@ -16,6 +19,16 @@ export default function Main() {
       <DeliveryService />
       <Magazine />
       <Promotion />
+      <button
+        onClick={onClickTopButton}
+        style={{ width: "1200px", height: "500px", fontSize: "50px" }}
+      >
+        탑버튼 기능입니다.
+        <br />
+        /src/components/units/main/index.tsx
+        <br />
+        여기에 있어요 요놈 옮겨야해요.
+      </button>
     </>
   );
 }
