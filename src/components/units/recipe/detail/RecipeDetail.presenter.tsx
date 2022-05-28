cf26979c0fb550a61d22da4976805057db66bd5d
KKingmo <55ooccmm@gmail.com>
2022-05-29 02:39:22 +0900

    Feat: [#212] 레시피 상세 일부분 구현, 댓글 폴더생성

--- src/components/units/recipe/detail/RecipeDetail.presenter.tsx
+++ src/components/units/recipe/detail/RecipeDetail.presenter.tsx
@@ -3,6 +3,7 @@ import * as RecipeDetail from "./RecipeDetail.styles";
 import Slider01 from "../../../../components/commons/sliders/01";
 import RecipeReview from "../detail/review/RecipeReview.container";
 import { useRouter } from "next/router";
+import { v4 as uuidv4 } from "uuid";
 
 export default function RecipeDetailUI(props) {
   const router = useRouter();
@@ -51,32 +52,37 @@ export default function RecipeDetailUI(props) {
           <RecipeDetail.Contents>
             <h2 ref={props.ingredientTabRef}>준비재료</h2>
             <RecipeDetail.Ingredient>
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((el) => (
-                <span key={el}>멸치 1컵(30g)</span>
-              ))}
+              {props.recipeData?.fetchRecipe?.ingredients
+                .map((el) => el.name)
+                .map((el) => (
+                  <span key={uuidv4()}>{el}</span>
+                ))}
             </RecipeDetail.Ingredient>
             <h2 ref={props.cookOrderTabRef} onClick={props.onClickCookOrderTab}>
               요리순서
             </h2>
-            {[1, 2, 3, 4].map((el) => (
-              <RecipeDetail.Order key={el}>
-                <span>Step {el}</span>
-                <RecipeDetail.ImageWrapper>
-                  <img
-                    src={`/img/recipeDetail/img-recipeDetail-0${el}.png`}
-                    alt=""
-                  />
-                </RecipeDetail.ImageWrapper>
+            {props.recipeData?.fetchRecipe?.recipesImages
+              .map((el) => el.url)
+              .map((el, index) => (
+                <RecipeDetail.Order key={uuidv4()}>
+                  <span>Step {index + 1}</span>
+                  <RecipeDetail.ImageWrapper>
+                    {el !== "" ? (
+                      <img src={`https://storage.googleapis.com/${el}`} />
+                    ) : (
+                      <div></div>
+                    )}
+                  </RecipeDetail.ImageWrapper>
 
-                <p>
-                  멸치를 넣고 공간이 넉넉한 접시 준비 잔멸치와 견과류,
-                  <br />
-                  포도씨유를 넣고 잘 섞어주세요. 전자레인지에 1분간
-                  <br />
-                  조리해주세요.
-                </p>
-              </RecipeDetail.Order>
-            ))}
+                  <p>
+                    멸치를 넣고 공간이 넉넉한 접시 준비 잔멸치와 견과류,
+                    <br />
+                    포도씨유를 넣고 잘 섞어주세요. 전자레인지에 1분간
+                    <br />
+                    조리해주세요.
+                  </p>
+                </RecipeDetail.Order>
+              ))}
           </RecipeDetail.Contents>
           <div ref={props.reviewTabRef}>
             <RecipeReview />
