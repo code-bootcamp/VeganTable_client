a1623995a85985f9100fb070c9532e55928c7307
hhjk00 <eunyu0604@gmail.com>
2022-05-30 01:38:41 +0900

    Feat: [#229] 회원 레시피 불러오기

--- src/components/units/myPage/myRecipe/MyRecipe.container.tsx
+++ src/components/units/myPage/myRecipe/MyRecipe.container.tsx
@@ -1,5 +1,14 @@
+import { useQuery } from "@apollo/client";
+import { FETCH_USER } from "../main/MyPageMain.queries";
 import MyRecipeUI from "./MyRecipe.presenter";
+import { FETCH_MY_RECIPE } from "./MyRecipe.queries";
 
 export default function MyRecipe() {
-  return <MyRecipeUI />;
+  const { data: userData } = useQuery(FETCH_USER);
+  const { data } = useQuery(FETCH_MY_RECIPE, {
+    variables: { user_id: String(userData?.fetchUser.user_id), page: 1 },
+  });
+  console.log(data);
+
+  return <MyRecipeUI data={data} />;
 }
--- src/components/units/subscribe/Subscribe.queries.ts
+++ src/components/units/subscribe/Subscribe.queries.ts
@@ -2,18 +2,12 @@ import { gql } from "@apollo/client";
 
 export const CREATE_BASIC_PAYMENT = gql`
   mutation createBasicPayment($impUid: String!, $amount: Float!) {
-    createBasicPayment(impUid: $impUid, amount: $amount) {
-      id
-      impUid
-    }
+    createBasicPayment(impUid: $impUid, amount: $amount)
   }
 `;
 
 export const CREATE_PREMIUM_PAYMENT = gql`
   mutation createPremiumPayment($impUid: String!, $amount: Float!) {
-    createPremiumPayment(impUid: $impUid, amount: $amount) {
-      id
-      impUid
-    }
+    createPremiumPayment(impUid: $impUid, amount: $amount)
   }
 `;
